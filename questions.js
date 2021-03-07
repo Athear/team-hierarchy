aed13fec72419dcbe7aff7b6f36af6ba47be72c5
Todd Malone <toddomalone@gmail.com>
2021-03-07 15:29:58 -0600

    set up main list of inquier questions

---
+++ questions.js
@@ -0,0 +1,47 @@
+const mainList = [
+    "View all employees",
+    "View employees by role",
+    "View employees by manager",
+    "View roles",
+    "View departments",
+    "Manage employees",
+    "Manage organization"
+]
+
+const employeeList = [
+    "Add employee",
+    "Delete employee",
+    "Update employee role",
+    "Update employee manager"
+]
+
+const orgList = [
+    "Add role",
+    "Delete role",
+    "Add department",
+    "Delete department"
+]
+
+
+const questions =[
+    {
+        type:"list",
+        name:"main",
+        message: "What would you like to do?",
+        choices: mainList
+    },
+    {
+        type:"list",
+        name:"empAct",
+        message: "Select employee action",
+        choices: employeeList,
+        when: function(answers){return answers.main==="Manage employees"}
+    },
+    {
+        type:"list",
+        name:"orgAct",
+        message: "Select organization action",
+        choices: employeeList,
+        when: function(answers){return answers.main==="Manage organization"}
+    }
+]
