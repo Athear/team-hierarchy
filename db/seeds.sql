09b94b5feeb9c0b89e7aaf5f4c019d555a330cf8
Todd Malone <toddomalone@gmail.com>
2021-03-08 20:15:42 -0600

    added employee seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -25,5 +25,16 @@ INSERT INTO role (title,salary,department_id) VALUES
 ("Junior Developer",40000,4);
 
 INSERT INTO employee (first_name,last_name,role_id) VALUES
+("Eliott","Woodward",1),
+("Courteney","Neville",6),
+("Olivier","Currie",7),
+("Melissa","Mclaughlin",9);
 
 INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES
+("Jensen","Jackson",2,1),
+("Orla","Redman",3,1),
+("Kamile","Warren",4,2),
+("Daniele","Ruiz",8,3),
+("Garry","Blankenship",8,3),
+("Roland","Roth",11,4),
+("Aqeel","Carter",10,4);
